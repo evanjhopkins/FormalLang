0b53eca5b4a536ecd96eb83c84f3503d54609239
popcat <moviemaker7@gmail.com>
2016-02-15 22:38:26 -0500

    switching input format to comman line arguments

--- driverDFA.java
+++ driverDFA.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class driverDFA {
 	public static void main(String args[]) {
 		boolean solutionIsValid = false;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Propose Solution: ");
-		String raw_input = scanner.nextLine();
+		// Scanner scanner = new Scanner(System.in);
+		// System.out.println("Propose Solution: ");
+
+		//String raw_input = scanner.nextLine();
+		String raw_input = args[0];
 		ManWolf mwInstance = new ManWolf();
-		solutionIsValid = mwInstance.evaluate(raw_input);
+		solutionIsValid = mwInstance.evalua te(raw_input);
 
 		if(solutionIsValid) {
 			System.out.println("That is a solution.");
